ed84b3c0bb6da2e8a27815fc120e319071e0e0e9
chaub123 <kinchau@hotmail.com.hk>
2025-02-09 16:00:25 +0800

    update

--- helper/src/main/java/com/chaub/Main.java
+++ helper/src/main/java/com/chaub/Main.java
@@ -1,7 +1,14 @@
 package com.chaub;
 
+import com.chaub.exception.InvalidInputException;
+import com.chaub.marksix.MarkSixHelper;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws InvalidInputException {
+        MarkSixHelper markSixHelper = new MarkSixHelper();
+
+        System.out.println(markSixHelper.single());
+        System.out.println(markSixHelper.single());
+        System.out.println(markSixHelper.multiple(8));
     }
 }
--- helper/src/main/java/com/chaub/marksix/MarkSixHelper.java
+++ helper/src/main/java/com/chaub/marksix/MarkSixHelper.java
@@ -1,10 +1,37 @@
 package com.chaub.marksix;
 
+import java.util.List;
 import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
-public class MarkSixHelper {
+import com.chaub.exception.InvalidInputException;
+
+public class MarkSixHelper{
     private static final int MIN_NUM = 1;
     private static final int MAX_NUM = 49;
     private static final int NUM_COUNT = 6;
     private static final Random random = new Random();
+
+    public List<Integer> single(){
+        return generate(NUM_COUNT);
+    }
+
+    public List<Integer> multiple(int count) throws InvalidInputException {
+
+        if(count < NUM_COUNT || count > MAX_NUM){
+            throw new InvalidInputException(String.format("The count is out of bound, should be between %d to %d", NUM_COUNT, MAX_NUM));
+        }
+        
+        return generate(count);
+    }
+
+    private List<Integer> generate(int count){
+        return IntStream.generate(() -> random.nextInt(MAX_NUM) + 1)
+                        .distinct()
+                        .limit(count)
+                        .boxed()
+                        .sorted()
+                        .collect(Collectors.toList());
+    }
 }
