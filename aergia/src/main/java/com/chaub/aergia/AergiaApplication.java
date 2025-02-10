aa6ae74868624319e356ab1179922555925a164c
chaub123 <kinchau@hotmail.com.hk>
2025-02-10 22:23:47 +0800

    aergia

---
+++ aergia/src/main/java/com/chaub/aergia/AergiaApplication.java
@@ -0,0 +1,13 @@
+package com.chaub.aergia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AergiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AergiaApplication.class, args);
+	}
+
+}
---
+++ aergia/src/test/java/com/chaub/aergia/AergiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chaub.aergia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AergiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
